86f370b831d9ada89f25ae901c8ed94187c78274
Brian-Webb <webbsdomain@gmail.com>
2017-10-16 13:03:34 -0700

    Fixing narration button styling

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,45 @@ import Creepypasta from './Creepypasta';
 import './app.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      "posts": null,
+      "currentPost": null
+    };
+
+    this.handleCurrentPostClick = this.handleCurrentPostClick.bind(this);
+
+    if(! this.state.posts) {
+      fetch('https://api.creepypastachamber.com/wp-json/wp/v2/creepypasta')
+        .then(data => data.json())
+        .then(data => this.setState({"posts": data}) );
+    }
+  }
+  handleCurrentPostClick(post) {
+    this.setState({"currentPost": post});
+
+    return true;
+  }
+
   render() {
+    window.scrollTo(0, 0);
     document.title = "Creepypasta Chamber";
 
     return (
       <div className="App">
         <section className="content">
           <Switch>
-            <Route exact path='/' component={CreepypastaList}/>
-            <Route path='/creepypasta/:string' component={Creepypasta}/>
+            <Route 
+              exact 
+              path='/'
+              render={(props) => <CreepypastaList {...props} posts={this.state.posts} handleCurrentPostClick={this.handleCurrentPostClick} />} 
+            />
+
+            <Route 
+              path='/creepypasta/:string'
+              render={(props) => <Creepypasta {...props} currentPost={this.state.currentPost} />} 
+            />
           </Switch>
         </section>
       </div>
